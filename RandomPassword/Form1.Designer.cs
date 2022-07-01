4f8b4c4844d84629ce77f10ce5f2d65ff999874b
Skynet <nathapoj@gmail.com>
2022-07-01 11:43:26 +0700

    Add project files.

---
+++ RandomPassword/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace RandomPassword
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Randombtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(68, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 91);
+            this.label1.TabIndex = 0;
+            // 
+            // Randombtn
+            // 
+            this.Randombtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Randombtn.Location = new System.Drawing.Point(41, 161);
+            this.Randombtn.Name = "Randombtn";
+            this.Randombtn.Size = new System.Drawing.Size(305, 105);
+            this.Randombtn.TabIndex = 1;
+            this.Randombtn.Text = "Random";
+            this.Randombtn.UseVisualStyleBackColor = true;
+            this.Randombtn.Click += new System.EventHandler(this.Randombtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 307);
+            this.Controls.Add(this.Randombtn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Randombtn;
+    }
+}
+
---
+++ RandomPassword/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomPassword
+{
+    public partial class Form1 : Form
+    {
+        string Password;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Randombtn_Click(object sender, EventArgs e)
+        {
+            int length;
+            Random random = new Random();
+            String dataset = "abcdefghijklmnopqrstuvwxyz0123456789";
+            length = 3;
+            Password = "";
+            for (int i = 0; i < length; i++)
+            {
+                int datasetpositon = random.Next(dataset.Length);
+                Password = Password + dataset.ElementAt(datasetpositon);                
+            }
+            label1.Text = Password;
+        }
+    }
+
+
+}
